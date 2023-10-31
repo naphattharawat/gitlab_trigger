ace1c51c84285fc32d3fcba579fe690f590bebcb
Naphattharawat S <naphattharawat@gmail.com>
2023-10-31 15:04:57 +0700

    update

--- typings.d.ts
+++ typings.d.ts
@@ -3,7 +3,6 @@ import * as Knex from 'knex';
 declare module 'express' {
   interface Request {
     db: any // Actually should be something like `multer.Body`
-    knex: Knex,
     decoded: any // Actually should be something like `multer.Files`
   }
 }
